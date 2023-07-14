2ff54e1a4ac4a98946c2a6ff7280658892a73946
bzavolovich <bzavolovich@gmail.com>
2023-07-14 21:27:13 +0300

    val_loss

--- data.py
+++ data.py
@@ -106,7 +106,7 @@ def unpack_tif(train_path, tif_folder, label_folder, target_size=(256, 256)):
             # cv2.imwrite(save_path, frame)
 
 
-def adjust_data(img, mask, flag_multi_class, num_class):
+def adjust_data(img, mask):
 
     """
     Adjusts the given image and mask according to the specified parameters.
@@ -114,28 +114,12 @@ def adjust_data(img, mask, flag_multi_class, num_class):
     Args:
         img (numpy.ndarray): The input image.
         mask (numpy.ndarray): The input mask.
-        flag_multi_class (bool): A flag indicating whether the mask contains multiple classes or not.
-        num_class (int): The number of classes in the mask.
 
     Returns:
         A tuple of the adjusted image and mask.
     """
 
-    if flag_multi_class:
-        img = img / 255
-        mask = mask[:, :, :, 0] if (len(mask.shape) == 4) else mask[:, :, 0]
-        new_mask = np.zeros(mask.shape + (num_class,))
-        for i in range(num_class):
-            # for one pixel in the image, find the class in mask and convert it into one-hot vector
-            # index = np.where(mask == i)
-            # index_mask = (index[0],index[1],index[2],np.zeros(len(index[0]),dtype = np.int64) + i) if (len(mask.shape) == 4) else (index[0],index[1],np.zeros(len(index[0]),dtype = np.int64) + i)
-            # new_mask[index_mask] = 1
-            new_mask[mask == i, i] = 1
-        new_mask = np.reshape(new_mask, (new_mask.shape[0], new_mask.shape[1] * new_mask.shape[2],
-                                         new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask, (
-            new_mask.shape[0] * new_mask.shape[1], new_mask.shape[2]))
-        mask = new_mask
-    elif (np.max(img) > 1):
+    if (np.max(img) > 1):
         img = img / 255
         mask = mask / 255
         mask[mask > 0.5] = 1
@@ -145,7 +129,7 @@ def adjust_data(img, mask, flag_multi_class, num_class):
 
 def training_dataset_generator(batch_size, train_path, image_folder, mask_folder, image_color_mode="grayscale",
                                mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
-                               flag_multi_class=False, num_class=2, save_to_dir=None, target_size=(256, 256), seed=1):
+                               save_to_dir=None, target_size=(256, 256), seed=1):
     """
     Generates batches of augmented data for training a model.
 
@@ -196,12 +180,12 @@ def training_dataset_generator(batch_size, train_path, image_folder, mask_folder
     train_generator = zip(image_generator, mask_generator)
 
     for (img, mask) in train_generator:
-        img, mask = adjust_data(img, mask, flag_multi_class, num_class)
+        img, mask = adjust_data(img, mask)
         yield img, mask
 
 
 
-def test_dataset_generator(test_path, target_size=(256, 256), flag_multi_class=False, as_gray=True):
+def test_dataset_generator(test_path, target_size=(256, 256), as_gray=True):
 
     """
     Generates test images as input for the model.
@@ -221,11 +205,11 @@ def test_dataset_generator(test_path, target_size=(256, 256), flag_multi_class=F
         img = io.imread(os.path.join(test_path, f), as_gray=as_gray)
         img = img / 255
         img = trans.resize(img, target_size)
-        img = np.reshape(img, img.shape + (1,)) if (not flag_multi_class) else img
+        img = np.reshape(img, img.shape + (1,))
         img = np.reshape(img, (1,) + img.shape)
         yield img
 
-def geneTrainNpy(image_path, mask_path, flag_multi_class=False, num_class=2, image_prefix="image", mask_prefix="mask",
+def geneTrainNpy(image_path, mask_path, image_prefix="image", mask_prefix="mask",
                  image_as_gray=True, mask_as_gray=True):
 
     """
@@ -256,7 +240,7 @@ def geneTrainNpy(image_path, mask_path, flag_multi_class=False, num_class=2, ima
         img = np.reshape(img, img.shape + (1,)) if image_as_gray else img
         mask = io.imread(item.replace(image_path, mask_path).replace(image_prefix, mask_prefix), as_gray=mask_as_gray)
         mask = np.reshape(mask, mask.shape + (1,)) if mask_as_gray else mask
-        img, mask = adjust_data(img, mask, flag_multi_class, num_class)
+        img, mask = adjust_data(img, mask)
         image_arr.append(img)
         mask_arr.append(mask)
     image_arr = np.array(image_arr)
